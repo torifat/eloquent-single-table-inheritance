9b3c780f33be626eb8da1ae38d762cab322457d6
Rifat Nabi <to.rifat@gmail.com>
2015-01-04 13:14:16 +0600

    fixed trait file name

--- src/SingleTableInheritanceTrait.php
+++ src/SingleTableInheritanceTrait.php
@@ -3,7 +3,8 @@
 namespace Rifat\EloquentSingleTableInheritance;
 
 
-trait SingleTableInheritanceTrait {
+trait SingleTableInheritanceTrait
+{
 
     /**
      * The ClassName where this Trait was invoked
@@ -37,7 +38,7 @@ trait SingleTableInheritanceTrait {
 
         if (get_class($this) !== static::$_stiBaseClass) {
             // TODO: Throws an exception if user tries to add a table name to sub-classes
-            $tableName =  str_replace('\\', '', snake_case(str_plural(class_basename(get_parent_class($this)))));
+            $tableName = str_replace('\\', '', snake_case(str_plural(class_basename(get_parent_class($this)))));
         } else {
             $tableName = str_replace('\\', '', snake_case(str_plural(class_basename($this))));
         }
@@ -63,7 +64,7 @@ trait SingleTableInheritanceTrait {
     /**
      * Save the model to the database.
      *
-     * @param  array  $options
+     * @param  array $options
      * @return bool
      */
     public function save(array $options = array())
@@ -75,7 +76,7 @@ trait SingleTableInheritanceTrait {
     /**
      * Create a new model instance that is existing.
      *
-     * @param  array  $attributes
+     * @param  array $attributes
      * @return static
      */
     public function newFromBuilder($attributes = array())
@@ -83,7 +84,7 @@ trait SingleTableInheritanceTrait {
         $class = $attributes->{static::$stiClassNameField};
         $instance = new $class;
         $instance->exists = true;
-        $instance->setRawAttributes((array) $attributes, true);
+        $instance->setRawAttributes((array)$attributes, true);
         return $instance;
 
     }
