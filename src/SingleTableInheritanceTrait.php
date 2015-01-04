c260c0ab773be55650c2cbdea7679130ee5534e5
Rifat Nabi <to.rifat@gmail.com>
2015-01-04 13:49:14 +0600

    simplified getTable

--- src/SingleTableInheritanceTrait.php
+++ src/SingleTableInheritanceTrait.php
@@ -36,14 +36,10 @@ trait SingleTableInheritanceTrait
     {
         if (isset($this->table)) return $this->table;
 
-        if (get_class($this) !== static::$_stiBaseClass) {
-            // TODO: Throws an exception if user tries to add a table name to sub-classes
-            $tableName = str_replace('\\', '', snake_case(str_plural(class_basename(get_parent_class($this)))));
-        } else {
-            $tableName = str_replace('\\', '', snake_case(str_plural(class_basename($this))));
-        }
-        $this->setTable($tableName);
-        return $tableName;
+        // Setting the value so that it doesn't evaluate each time
+        $this->table = str_replace('\\', '', snake_case(str_plural(class_basename(static::$_stiBaseClass))));
+
+        return $this->table;
     }
 
     /**
